644bd1830fd4fa9fb91f1779298c3f33d6acbe39
deenharo01 <decentnharo01@gmail.com>
2024-11-02 21:17:44 +0200

    Update config.js
    Bot by deenharo01

--- config.js
+++ config.js
@@ -5,6 +5,6 @@ function convertToBool(text, fault = 'true') {
     return text === fault ? true : false;
 }
 module.exports = {
-SESSION_ID: process.env.SESSION_ID || "",
+SESSION_ID: process.env.SESSION_ID || "𝙰𝚂𝙸𝚃𝙷𝙰-𝙼𝙳=qyRVjbIR#cR7ds_KN0eA90nylLTSGaJgBt92Z41z8XvL3PhM6L78",
 MONGODB: process.env.MONGODB || "",//enter mongo db url හදන විදිය පල්ලෙහාබටන් එකක් ඇති
 };
